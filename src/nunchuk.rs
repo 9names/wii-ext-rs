bbb5e2a8e1e62ce22d38789570f271f6de04e9c2
9names <60134748+9names@users.noreply.github.com>
2022-02-17 21:26:17 +1100

    Add tests for Calibrated Nunchuk joystick movement

--- src/nunchuk.rs
+++ src/nunchuk.rs
@@ -200,6 +200,14 @@ where
         self.read_nunchuk()
     }
 
+    /// Simple helper with no delay. Should work for testing, not sure if it will function on hardware
+    pub fn read_calibrated_no_wait(&mut self) -> Result<NunchukReadingCalibrated, Error<E>> {
+        Ok(NunchukReadingCalibrated::new(
+            self.read_no_wait()?,
+            &self.calibration,
+        ))
+    }
+
     /// Simple blocking read helper that will start a sample, wait 10ms, then read the value
     pub fn read_blocking<D: DelayUs<u16>>(
         &mut self,
@@ -227,6 +235,11 @@ mod tests {
     use super::*;
     use crate::test_data;
     use embedded_hal_mock::i2c::{self, Transaction};
+    /// There's a certain amount of slop around the center position.
+    /// Allow up to this range without it being an error
+    const ZERO_SLOP: i8 = 5;
+    /// The max value at full deflection is ~100, but allow a bit less than that
+    const AXIS_MAX: i8 = 90;
 
     // TODO: work out how to test analogue values from joystick and gyro
 
@@ -244,6 +257,120 @@ mod tests {
         let report = nc.read_no_wait().unwrap();
         assert!(!report.c_button_pressed);
         assert!(!report.z_button_pressed);
+    }
+
+    #[test]
+    fn nunchuck_idle_calibrated() {
+        let expectations = vec![
+            Transaction::write(EXT_I2C_ADDR as u8, vec![0]),
+            Transaction::read(EXT_I2C_ADDR as u8, test_data::NUNCHUCK_IDLE.to_vec()),
+        ];
+        let mock = i2c::Mock::new(&expectations);
+        let idle = NunchukReading::from_data(&test_data::NUNCHUCK_IDLE).unwrap();
+        let mut nc = Nunchuk {
+            i2cdev: mock,
+            calibration: CalibrationData {
+                joystick_x: idle.joystick_x,
+                joystick_y: idle.joystick_y,
+            },
+        };
+        let report = nc.read_calibrated_no_wait().unwrap();
+        assert!(!report.c_button_pressed);
+        assert!(!report.z_button_pressed);
+        assert_eq!(report.joystick_x, 0);
+        assert_eq!(report.joystick_y, 0);
+    }
+
+    #[test]
+    fn nunchuck_left_calibrated() {
+        let expectations = vec![
+            Transaction::write(EXT_I2C_ADDR as u8, vec![0]),
+            Transaction::read(EXT_I2C_ADDR as u8, test_data::NUNCHUCK_JOY_L.to_vec()),
+        ];
+        let mock = i2c::Mock::new(&expectations);
+        let idle = NunchukReading::from_data(&test_data::NUNCHUCK_IDLE).unwrap();
+        let mut nc = Nunchuk {
+            i2cdev: mock,
+            calibration: CalibrationData {
+                joystick_x: idle.joystick_x,
+                joystick_y: idle.joystick_y,
+            },
+        };
+        let report = nc.read_calibrated_no_wait().unwrap();
+        assert!(!report.c_button_pressed);
+        assert!(!report.z_button_pressed);
+        assert!(report.joystick_x < -AXIS_MAX, "x = {}", report.joystick_x);
+        assert!(report.joystick_y > -ZERO_SLOP, "y = {}", report.joystick_y);
+        assert!(report.joystick_y < ZERO_SLOP, "y = {}", report.joystick_y);
+    }
+
+    #[test]
+    fn nunchuck_right_calibrated() {
+        let expectations = vec![
+            Transaction::write(EXT_I2C_ADDR as u8, vec![0]),
+            Transaction::read(EXT_I2C_ADDR as u8, test_data::NUNCHUCK_JOY_R.to_vec()),
+        ];
+        let mock = i2c::Mock::new(&expectations);
+        let idle = NunchukReading::from_data(&test_data::NUNCHUCK_IDLE).unwrap();
+        let mut nc = Nunchuk {
+            i2cdev: mock,
+            calibration: CalibrationData {
+                joystick_x: idle.joystick_x,
+                joystick_y: idle.joystick_y,
+            },
+        };
+        let report = nc.read_calibrated_no_wait().unwrap();
+        assert!(!report.c_button_pressed);
+        assert!(!report.z_button_pressed);
+        assert!(report.joystick_x > AXIS_MAX, "x = {}", report.joystick_x);
+        assert!(report.joystick_y > -ZERO_SLOP, "y = {}", report.joystick_y);
+        assert!(report.joystick_y < ZERO_SLOP, "y = {}", report.joystick_y);
+    }
+
+    #[test]
+    fn nunchuck_up_calibrated() {
+        let expectations = vec![
+            Transaction::write(EXT_I2C_ADDR as u8, vec![0]),
+            Transaction::read(EXT_I2C_ADDR as u8, test_data::NUNCHUCK_JOY_U.to_vec()),
+        ];
+        let mock = i2c::Mock::new(&expectations);
+        let idle = NunchukReading::from_data(&test_data::NUNCHUCK_IDLE).unwrap();
+        let mut nc = Nunchuk {
+            i2cdev: mock,
+            calibration: CalibrationData {
+                joystick_x: idle.joystick_x,
+                joystick_y: idle.joystick_y,
+            },
+        };
+        let report = nc.read_calibrated_no_wait().unwrap();
+        assert!(!report.c_button_pressed);
+        assert!(!report.z_button_pressed);
+        assert!(report.joystick_y > AXIS_MAX, "y = {}", report.joystick_y);
+        assert!(report.joystick_x > -ZERO_SLOP, "x = {}", report.joystick_x);
+        assert!(report.joystick_x < ZERO_SLOP, "x = {}", report.joystick_x);
+    }
+
+    #[test]
+    fn nunchuck_down_calibrated() {
+        let expectations = vec![
+            Transaction::write(EXT_I2C_ADDR as u8, vec![0]),
+            Transaction::read(EXT_I2C_ADDR as u8, test_data::NUNCHUCK_JOY_D.to_vec()),
+        ];
+        let mock = i2c::Mock::new(&expectations);
+        let idle = NunchukReading::from_data(&test_data::NUNCHUCK_IDLE).unwrap();
+        let mut nc = Nunchuk {
+            i2cdev: mock,
+            calibration: CalibrationData {
+                joystick_x: idle.joystick_x,
+                joystick_y: idle.joystick_y,
+            },
+        };
+        let report = nc.read_calibrated_no_wait().unwrap();
+        assert!(!report.c_button_pressed);
+        assert!(!report.z_button_pressed);
+        assert!(report.joystick_y < -AXIS_MAX, "y = {}", report.joystick_y);
+        assert!(report.joystick_x > -ZERO_SLOP, "x = {}", report.joystick_x);
+        assert!(report.joystick_x < ZERO_SLOP, "x = {}", report.joystick_x);
     }
 
     #[test]
