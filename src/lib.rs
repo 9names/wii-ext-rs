30edc6c33dfdf95609efeb98826bfcaf6af1330d
9names <60134748+9names@users.noreply.github.com>
2022-02-19 18:52:04 +1100

    Make test_data pub visible to get tests working again

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 #![cfg_attr(not(test), no_std)]
 pub mod classic;
 pub mod nunchuk;
-
 /// Standard input or ID report
 pub type ExtReport = [u8; 6];
 /// HD input report
@@ -15,5 +14,6 @@ pub const EXT_I2C_ADDR: u16 = 0x52;
 /// 200 microseconds works in my tests - need to test with more devices
 pub const INTERMESSAGE_DELAY_MICROSEC: u16 = 200;
 
-// #[cfg(test)]
+/// Test data used by the integration tests to confirm that the driver is working.  
+/// Not intended for use outside of this crate.
 pub mod test_data;
